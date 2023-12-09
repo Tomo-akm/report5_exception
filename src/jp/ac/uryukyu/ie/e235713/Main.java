212d8ce6a9f53ea48ccc7410d1a8080e61a46589
Tomonori Akamine <e235713@ie.u-ryukyu.ac.jp>
2023-12-09 19:11:16 +0900

    check NumberFormatException

--- src/jp/ac/uryukyu/ie/e235713/Main.java
+++ src/jp/ac/uryukyu/ie/e235713/Main.java
@@ -2,12 +2,7 @@ package jp.ac.uryukyu.ie.e235713;
 
 public class Main {
     public static void main(String[] args) {
-        try {
-            String str = null;
-            System.out.println(str.length());
-        } catch (NullPointerException e) {
-            System.out.println("エラーが発生しました。");
-            System.out.println(e.getMessage());
-        }
+        String str = "壱百満";
+        int value = Integer.parseInt(str);
     }    
 }
